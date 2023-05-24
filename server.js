374ad79673aa17e115a916b7b32f39a3dfe907ed
mamadou <amadou.mbodj@ynov.com>
2023-05-24 11:15:59 +0200

    server.js

--- server.js
+++ server.js
@@ -1,8 +1,36 @@
 const express = require('express');
 const app = express();
+
 app.get('/', (req, res) => {
-    res.send('Hello, World!');
+    res.sendFile(__dirname + '/Html.html');
+
 });
+
 app.listen(3000, () => {
     console.log('Server is running on port 3000');
+});
+app.get('/Html', (req, res) => {
+    const request = require('request');
+    const cheerio = require('cheerio');
+
+    const url = 'https://www.wiflix.surf/';
+
+    request(url, (error, response, html) => {
+        if (!error && response.statusCode == 200) {
+            const $ = cheerio.load(html);
+            const links = $('a');
+            const linkList = [];
+
+            links.each((i, link) => {
+                const href = $(link).attr('href');
+                if (href) {
+                    linkList.push(href);
+                }
+            });
+
+            res.json(linkList); // Envoie la liste des liens en tant que réponse JSON
+        } else {
+            res.status(500).send('Une erreur est survenue lors de la récupération des liens.');
+        }
+    });
 });
