67dab4e2feae548a7c22edb770e16d26360993b7
mamadou <amadou.mbodj@ynov.com>
2023-05-26 13:58:32 +0200

    pageWeb.js

--- showDate.js
+++ showDate.js
@@ -44,3 +44,39 @@ reset.addEventListener('click', event => {
     console.log('Forms reset.');
     return false;
 });
+
+//Confirm button
+const confirm = document.getElementById('confirm');
+
+confirm.addEventListener('click', event => {
+
+    event.preventDefault();
+
+    for (let i = 0; i < inputs.length; i++) {
+        inputs[i].className = '';
+        inputs[i].value = '';
+    }
+
+    return false;
+});
+
+
+
+// Récupérer le bouton "Confirm"
+    var confirmButton = document.querySelector("#confirm");
+
+    // Ajouter un gestionnaire d'événement lorsque le bouton "Confirm" est cliqué
+    confirmButton.addEventListener("click", function(event) {
+        event.preventDefault(); // Empêcher le comportement par défaut du bouton (rechargement de la page)
+
+        // Récupérer les valeurs des champs de formulaire
+        var month = document.querySelector("#options").value;
+        var year = document.querySelector("input[name='year']").value;
+
+        // Afficher les valeurs
+        var resultContainer = document.createElement("div");
+        resultContainer.textContent = "Month: " + month + ", Year: " + year;
+        document.body.appendChild(resultContainer);
+    });
+
+
