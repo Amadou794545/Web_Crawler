10246af13416879c1198246ed3a71c808228a60a
edgar <edgar.andre@ynov.com>
2023-05-26 14:54:24 +0200

    index.html fixed1

--- static/js/server.js
+++ static/js/server.js
@@ -1,36 +1,14 @@
-const express = require('express');
+import express from "express";
+import { dirname } from 'path';
+import { fileURLToPath } from 'url';
+import * as path from "path";
 const app = express();
-
+const __dirname = dirname(fileURLToPath(import.meta.url));
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-
+    res.sendFile(__dirname + "\\index.html")
+    // res.sendFile(path.join('Web/template/start.html'));
 });
-
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
-});
-app.get('/Html', (req, res) => {
-    const request = require('request');
-    const cheerio = require('cheerio');
-
-    const url = 'https://www.wiflix.surf/';
-
-    request(url, (error, response, html) => {
-        if (!error && response.statusCode == 200) {
-            const $ = cheerio.load(html);
-            const links = $('a');
-            const linkList = [];
-
-            links.each((i, link) => {
-                const href = $(link).attr('href');
-                if (href) {
-                    linkList.push(href);
-                }
-            });
-
-            res.json(linkList); // Envoie la liste des liens en tant que réponse JSON
-        } else {
-            res.status(500).send('Une erreur est survenue lors de la récupération des liens.');
-        }
-    });
+const port = 3000;
+app.listen(port, () => {
+    console.log(`Server is listening on port ${port}`);
 });
